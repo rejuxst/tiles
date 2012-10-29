5e0b8324ea89689528756b97717afd571dc8a905
preator <null>
2012-10-29 16:32:31 -0400

    Testing suite class generated Ling and Database updated to reflect that

--- test/blocktest/database_test.rb
+++ test/blocktest/database_test.rb
@@ -1,123 +1,91 @@
 require 'pry'
 
-def non_interactive?
-	return true
-end
-def require_from_source
-	$LOAD_PATH << File.absolute_path(File.join(File.dirname(__FILE__),'/../../lib/'))
-	core = File.join(File.dirname(__FILE__),"..","..","lib")
-	#puts "$LOAD_PATH LIST:"
-	#puts $LOAD_PATH
-	Dir.open(core) do |ent|
-		ent.entries.each do |f|
-		unless File.directory?(File.join(core,f)) || !(f.match(/\.gitignore/).nil?) || !(f.match(/\.swp/).nil?)
-			succ = gem_original_require File.expand_path File.join(ent.to_path,f.partition('.')[0])
+class Test_Database < Test
+	register_test :test_valid_tree
+	register_test :test_add_remove
+	def tree3
+		g = TDB.new
+		2.times { g.add_to_db(TDB.new) }
+		g.for_each_db_entry { |e| 2.times { e.add_to_db(TDB.new) } }
+		g.for_each_db_entry do |e|
+			e.for_each_db_entry { |f| 2.times { f.add_to_db(TDB.new) } }
 		end
-		end
-	end
-rescue
-        binding.pry
-end
-def create_global_database
-	$global_db = TDB.new
-	$all_things = [$global_db]
-end
-def create_global(a)
-	$all_things << a
-	global_add_to_db(a)
-end
-def global_add_to_db(a)
-	$global_db.add_to_db(a)
-end
-def global_remove_from_db(a)
-	$global_db.remove_from_db(a)
-end
-def global_destroy(a)
-	$global_db.destroy_entry(a)
-end
-def tree3
-	g = TDB.new
-	2.times { g.add_to_db(TDB.new) }
-	g.for_each_db_entry { |e| 2.times { e.add_to_db(TDB.new) } }
-	g.for_each_db_entry do |e|
-		e.for_each_db_entry { |f| 2.times { f.add_to_db(TDB.new) } }
-	end
-	return g
-rescue
-	binding.pry
-end
-def print_tree(tree,tablvl = 0)
-	puts "=" + ">" * tablvl + "#{tree.key} = #{tree.db_alive ? "alive" : "dead"}"
-	tree.for_each_instance { |e| print_tree(e,tablvl+1)}
-	return nil
-end
-def test_add_remove
-	puts "Testing add_to_db and remove_from_db"
-	t3 = tree3
-	list = []
-	list << t3
-	t3.for_each_db_entry { |e| list << e }
-	t3.for_each_db_entry { |e| e.for_each_db_entry { |f| list << f} }
-	t3.for_each_db_entry { |e| e.for_each_db_entry { |f|   f.for_each_db_entry { |g| list << g} } }
-	# Size check: add_to_db works
-	if(list.length == 15)
-		puts "\tCorrect number of elements in a binary tree depth 3 (which is 15): pass"
-	else
-		raise "Incorrect number of element in binary tree of depth 3 #{list.length}: fail"
-	end
-	# Singular destruction: destroy_self on leaf Database works
-	list[7].destroy_self
-	sum = 0
-	list.each { |l| sum = sum +1 if l.db_alive } 
-	puts "\tTesting singular delete: #{(!list[7].db_alive && sum == 14) ? "pass" : "fail" }"
-	# Singluar Destruction: Destroy a branch
-	list[4].destroy_self
-	sum = 0
-	list.each { |l| sum = sum +1 if l.db_alive } 
-	puts "\tTesting branch delete: #{(!list[7].db_alive && sum == 11 && !list[4].db_alive && !list[9].db_alive && !list[10].db_alive) ? "pass" : "fail" }"
-	# Singluar Destruction: Destroy a nested branch
-	list[2].destroy_self
-	sum = 0
-	list.each { |l| sum = sum +1 if l.db_alive } 
-	large_pass = sum == 4 && !list[7].db_alive && !list[4].db_alive && !list[9].db_alive && !list[10].db_alive &&  !list[5].db_alive 
-	large_pass = large_pass && !list[6].db_alive &&  !list[11].db_alive &&  !list[12].db_alive && !list[13].db_alive && !list[14].db_alive
-	puts "\tTesting large branch delete: #{(large_pass)? "pass" : "fail" }"
-rescue
-	binding.pry
-end
-def valid_parent_child(par,chd)
-	return (chd.db_parent == par && par[chd.key] == chd && par.find_key(chd) == chd.key)? true : false
-end
-def test_valid_tree
-	puts "Test if Tree depth 3 is a valid databse tree: #{(rec_test_valid_tree(tree3()))? "pass": "fail" }"
-end
-def rec_test_valid_tree(tree)
-	valid = true
-	tree.for_each_instance do |x| 
-		valid = valid && valid_parent_child(tree,x)
-		valid = valid && rec_test_valid_tree(x) 
-	end
-	return valid
-end
-###########
-require_from_source    # Load the Tiles Core (Syntax Test Bench)
-class TDB 
-	include Database
-	def initialize()
-		init_database
-	end
-end
-begin
-	#create_global_database
-	#5.times {|x| create_global(TDB.new) }
-	puts "============ Testing Database module ====================="
-	test_valid_tree
-	test_add_remove
-	puts "============ Finished Testing ====================="
-	unless non_interactive? # Alias global to local
+		return g
+	rescue
 		binding.pry
 	end
-rescue
-	binding.pry
+	def print_tree(tree,tablvl = 0)
+		puts "=" + ">" * tablvl + "#{tree.key} = #{tree.db_alive ? "alive" : "dead"}"
+		tree.for_each_instance { |e| print_tree(e,tablvl+1)}
+		return nil
+	end
+	def test_add_remove
+		puts "Testing add_to_db and remove_from_db"
+		t3 = tree3
+		list = []
+		list << t3
+		t3.for_each_db_entry { |e| list << e }
+		t3.for_each_db_entry { |e| e.for_each_db_entry { |f| list << f} }
+		t3.for_each_db_entry { |e| e.for_each_db_entry { |f|   f.for_each_db_entry { |g| list << g} } }
+		# Size check: add_to_db works
+		if(list.length == 15)
+			puts "\tCorrect number of elements in a binary tree depth 3 (which is 15): pass"
+		else
+			raise "Incorrect number of element in binary tree of depth 3 #{list.length}: fail"
+		end
+		# Singular destruction: destroy_self on leaf Database works
+		list[7].destroy_self
+		sum = 0
+		list.each { |l| sum = sum +1 if l.db_alive } 
+		puts "\tTesting singular delete: #{(!list[7].db_alive && sum == 14) ? "pass" : "fail" }"
+		# Singluar Destruction: Destroy a branch
+		list[4].destroy_self
+		sum = 0
+		list.each { |l| sum = sum +1 if l.db_alive } 
+		puts "\tTesting branch delete: #{(!list[7].db_alive && sum == 11 && !list[4].db_alive && !list[9].db_alive && !list[10].db_alive) ? "pass" : "fail" }"
+		# Singluar Destruction: Destroy a nested branch
+		list[2].destroy_self
+		sum = 0
+		list.each { |l| sum = sum +1 if l.db_alive } 
+		large_pass = sum == 4 && !list[7].db_alive && !list[4].db_alive && !list[9].db_alive && !list[10].db_alive &&  !list[5].db_alive 
+		large_pass = large_pass && !list[6].db_alive &&  !list[11].db_alive &&  !list[12].db_alive && !list[13].db_alive && !list[14].db_alive
+		puts "\tTesting large branch delete: #{(large_pass)? "pass" : "fail" }"
+	rescue
+		binding.pry
+	end
+	def valid_parent_child(par,chd)
+		return (chd.db_parent == par && par[chd.key] == chd && par.find_key(chd) == chd.key)? true : false
+	end
+	def test_valid_tree
+		puts "Test if Tree depth 3 is a valid databse tree: #{(rec_test_valid_tree(tree3()))? "pass": "fail" }"
+	end
+	def rec_test_valid_tree(tree)
+		valid = true
+		tree.for_each_instance do |x| 
+			valid = valid && valid_parent_child(tree,x)
+			valid = valid && rec_test_valid_tree(x) 
+		end
+		return valid
+	end
+	###########
+	#require_from_source    # Load the Tiles Core (Syntax Test Bench)
+	class TDB 
+		include ::Database
+		def initialize()
+			init_database
+		end
+	end
+#begin
+#	#create_global_database
+#	#5.times {|x| create_global(TDB.new) }
+#	puts "============ Testing Database module ====================="
+#	test_valid_tree
+#	test_add_remove
+#	puts "============ Finished Testing ====================="
+#	unless non_interactive? # Alias global to local
+#		binding.pry
+#	end
+#rescue
+#	binding.pry
+#end
 end
-
--- test/blocktest/linguistics_test.rb
+++ test/blocktest/linguistics_test.rb
@@ -1,77 +1,67 @@
 require 'pry'
-def non_interactive?
-	return false
-end
-def require_from_source
-	$LOAD_PATH << File.absolute_path(File.join(File.dirname(__FILE__),'/../../lib/'))
-	core = File.join(File.dirname(__FILE__),"..","..","lib")
-	#puts "$LOAD_PATH LIST:"
-	#puts $LOAD_PATH
-	Dir.open(core) do |ent|
-		ent.entries.each do |f|
-		unless File.directory?(File.join(core,f)) || !(f.match(/\.gitignore/).nil?) || !(f.match(/\.swp/).nil?)
-			succ = gem_original_require File.expand_path File.join(ent.to_path,f.partition('.')[0])
-#			puts "Requiring lib file: #{File.join(f.partition('.')[0])} => #{succ}"
-		end
-		end
+class Test_Linguistics < Test
+	register_test :load_bare_linguisitics
+	def add_to_dict(word,wordclass)
+		$dictionary = {} if $dictionary.nil?
+		$dictionary[word] = wordclass
 	end
-rescue
-        binding.pry
-end
-def add_to_dict(word,wordclass)
-	$dictionary = {} if $dictionary.nil?
-	$dictionary[word] = wordclass
-end
-def load_test_word_lib
-	add_to_dict("a",$indefinite_article)
-	add_to_dict("dog",$noun)
-	add_to_dict("ran",$verb)
-	add_to_dict("fast",$adverb)
-	add_to_dict("the",$definite_article)
-rescue
-	binding.pry	
-end
-def load_test_word_classes
-	$word_class_list = Hash.new()
-	$word_class_list[:definite_article] 	= Linguistics::WordClass.new(:definite_article, "ArticleNoun+")
-	$word_class_list[:indefinite_article] 	= Linguistics::WordClass.new(:indefinite_article, "ArticleNoun+")
-	$word_class_list[:noun]			= Linguistics::WordClass.new(:noun, "{ArticleNoun-} and NounVerb+")
-	$word_class_list[:verb]	 	    	= Linguistics::WordClass.new(:verb, "{AdverbVerb+} and NounVerb-")
-	$word_class_list[:adverb]	     	= Linguistics::WordClass.new(:adverb,"AdverbVerb-")
-	$word_class_list.each {|key,value| eval("$#{key.to_s} = value"); }
-rescue
-	puts "Errored out on load_test_word_lib refer to $word_class_list"
-	binding.pry
-end
-def load_test_sentences
-	begin
-	$sentences = []
-	$sentences.push Linguistics::Sentence.from_string("A dog ran.",$dictionary)
-	$sentences.push Linguistics::Sentence.from_string("The dog ran fast.",$dictionary)
-	$sentences.each do |local|
-		puts	"Sentence(\"#{local.to_string()}\") \n\tLink Table: #{local.create_linkages_table}"
-	end
+	def load_test_word_lib
+		add_to_dict("a",$indefinite_article)
+		add_to_dict("dog",$noun)
+		add_to_dict("ran",$verb)
+		add_to_dict("fast",$adverb)
+		add_to_dict("the",$definite_article)
 	rescue
+		binding.pry	
+	end
+	def load_test_word_classes
+		$word_class_list = Hash.new()
+		$word_class_list[:definite_article] 	= Linguistics::WordClass.new(:definite_article, "ArticleNoun+")
+		$word_class_list[:indefinite_article] 	= Linguistics::WordClass.new(:indefinite_article, "ArticleNoun+")
+		$word_class_list[:noun]			= Linguistics::WordClass.new(:noun, "{ArticleNoun-} and NounVerb+")
+		$word_class_list[:verb]	 	    	= Linguistics::WordClass.new(:verb, "{AdverbVerb+} and NounVerb-")
+		$word_class_list[:adverb]	     	= Linguistics::WordClass.new(:adverb,"AdverbVerb-")
+		$word_class_list.each {|key,value| eval("$#{key.to_s} = value"); }
+	rescue
+		puts "Errored out on load_test_word_lib refer to $word_class_list"
 		binding.pry
 	end
-	$sentences.each { |sen| sen.resolve rescue binding.pry }
-end
-def sentence_from_string(string)
-	arr = string.split(/[\., ]/).collect { |s| s.downcase }
-	output = Linguistics::Sentence.new
-	arr.each {|word| output.add_word Linguistics::Word.new($dictionary[word],word) }
-	return output
-rescue 
-	binding.pry
-end
-begin
-	require_from_source    # Load the Tiles Core (Syntax Test Bench)
-	load_test_word_classes # Load the Word Classes (Word Class Test Bench)
-	load_test_word_lib     # Load the Library    (Word / Connector Test Bench)
-	load_test_sentences    # Load Test Sentences (Sentence Parsing Test Bench)
-	unless non_interactive? # Alias global to local
+	def load_test_sentences
+		begin
+		$sentences = []
+		$sentences.push Linguistics::Sentence.from_string("A dog ran.",$dictionary)
+		$sentences.push Linguistics::Sentence.from_string("The dog ran fast.",$dictionary)
+		$sentences.each do |local|
+			puts	"Sentence(\"#{local.to_string()}\") \n\tLink Table: #{local.create_linkages_table}"
+		end
+		rescue
+			binding.pry
+		end
+		$sentences.each { |sen| sen.resolve rescue binding.pry }
+	end
+	def sentence_from_string(string)
+		arr = string.split(/[\., ]/).collect { |s| s.downcase }
+		output = Linguistics::Sentence.new
+		arr.each {|word| output.add_word Linguistics::Word.new($dictionary[word],word) }
+		return output
+	rescue 
 		binding.pry
 	end
-rescue
-	binding.pry
+	def load_bare_linguisitics
+		load_test_word_classes # Load the Word Classes (Word Class Test Bench)
+		load_test_word_lib     # Load the Library    (Word / Connector Test Bench)
+		load_test_sentences    # Load Test Sentences (Sentence Parsing Test Bench)	
+	end
 end
+#begin
+#	require_from_source    # Load the Tiles Core (Syntax Test Bench)
+#	load_bare_linguisitics
+#	load_test_word_classes # Load the Word Classes (Word Class Test Bench)
+#	load_test_word_lib     # Load the Library    (Word / Connector Test Bench)
+#	load_test_sentences    # Load Test Sentences (Sentence Parsing Test Bench)
+#	unless non_interactive? # Alias global to local
+#		binding.pry
+#	end
+#rescue
+#	binding.pry
+#end
