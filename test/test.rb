cea9533a4773c68127ef6b8bd1a83256f587caf9
rejuxst <wer123hitech@gmail.com>
2013-01-08 10:37:10 -0500

    New Reference form and updated test suite

--- test/test.rb
+++ test/test.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require 'test/unit'
 def require_from_source
 	$LOAD_PATH << File.absolute_path(File.join(File.dirname(__FILE__),'/../lib/'))
 	core = File.join(File.dirname(__FILE__),"..","lib")
@@ -14,13 +15,8 @@ def require_from_source
 rescue
 	binding.pry
 end
-class Test
+class Tiles_Test < Test::Unit::TestCase
 	@@test_func = {}
-	def self.register_test(*args)
-		args.each { |x| raise "register_test only accepts symbols" unless x.class <= Symbol }
-		@@test_func["#{self}"] = [] if @@test_func["#{self}"].nil?
-		args.each { |x| @@test_func["#{self}"] << x }	
-	end
 	def non_interactive?
 		return true
 	end
@@ -28,25 +24,14 @@ class Test
 	def enter_the_test_block
 		binding.pry
 	end
-	def self.run_test
-		it = self.new
-		puts "================ Testing using #{it.class} ================"
-		@@test_func["#{self}"].each do |x|
-			it.send(x)	
-		end if !@@test_func["#{self}"].nil?
-		puts "================ Finished Testing          ================"
-		unless it.non_interactive?
-			it.enter_the_test_block
-		end
-	rescue
-		binding.pry
-	end
 end
 begin
 	blkt = File.absolute_path(File.join(File.dirname(__FILE__),'/blocktest/'))
 	require_from_source
 	ARGV.each do |block|
-		load blkt +'/'+ block.downcase + "_test.rb"
-		eval('Test_' + block + '.run_test')
+		puts "================ Testing #{block} ================"
+		require blkt +'/'+ block.downcase + "_test.rb"
+		#eval('Test_' + block + '.run_test')
+		#puts "================ Finished #{block} ================"
 	end
 end
