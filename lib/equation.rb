3a604d682b188dd4c198dd5f3b46a9dd96018bf5
rejuxst <wer123hitech@gmail.com>
2013-02-04 20:29:56 -0500

    Updated equation and generic to have effects. Added to the app-test to support effects that change properties (if you walk through the water you lose 1 hp on the move). Noticed that there might be redundancy in the way :via/:target is handled from a idiomatic point of view. Should redunacy be handled by the game or should we leave that to coding practices?

--- lib/equation.rb
+++ lib/equation.rb
@@ -1,5 +1,7 @@
+require 'pry'
 require 'treetop'
-
+require 'polyglot'
+require 'treetop/equationparser.treetop'
 module MathEquation
 	class Equation  < Treetop::Runtime::SyntaxNode	
 		def value
@@ -8,12 +10,27 @@ module MathEquation
 		def source=(input)
 			@source = input
 		end
+		def source
+			@source
+		end
+		def has_target?
+			!output.empty?
+		end
+		def target
+			output.target if !output.empty?
+		end
+		def target_value
+			target.value(@source) if !output.empty?
+		end
 	end
 	class Operation < Treetop::Runtime::SyntaxNode
 		def value
 			other_terms.elements.inject(start.value) do |res,ele| 
 				res.send(ele.operator.text_value,ele.term.value) 
 			end
+		end
+		def source
+			parent.source
 		end
 	end
 	class Literal < Treetop::Runtime::SyntaxNode
@@ -26,6 +43,9 @@ module MathEquation
 		def value
 			text_value.to_i
 		end
+		def source
+			parent.source
+		end
 	end
 	class Variable < Treetop::Runtime::SyntaxNode
 			def is_variable?
@@ -34,25 +54,40 @@ module MathEquation
 			def is_literal?
 				false
 			end
-			def value
-				@source[text_value]
+			def value(src = nil)
+				text_value.strip.split('#').inject(src || parent.source) { |res,ele| res[ele] }
 			end
 	end
 end
 class Equation
-	Treetop.load File.join(File.dirname(__FILE__),'treetop/equationparser')
 	@@parser = MathEquationParser.new
 	def initialize(string)
 		@equation = @@parser.parse(string)
 	end
 	def source=(src)
-		@source = src
+		@equation.source = src
+	end
+	def parse_failure?
+		!@equation.nil?
 	end
 	def resolve
-		return @equation.value
+		unless @equation.has_target?
+			@equation.value
+		else
+			raise "No Source database given for equation with output" if @equation.source.nil?
+			@equation.target_value.set @equation.value
+		end
+	end
+	def to_s
+		@equation.text_value
 	end
 	def self.last_failure_reason
 		return @@parser.failure_reason
 	end
 end
-
+class Effect < Equation
+	def resolve(src)
+		self.source = src
+		super()
+	end
+end
--- test/blocktest/equation_test.rb
+++ test/blocktest/equation_test.rb
@@ -1,6 +1,7 @@
 require 'pry'
 class Test_Equation < Tiles_Test
 	def test_simple_equations
+		
 		assert_equal 3	,Equation.new('2+1').resolve	
 		assert_equal 3	,Equation.new('(2+1)').resolve	
 		assert_equal 5	,Equation.new(' 4 + 1').resolve
@@ -13,5 +14,28 @@ class Test_Equation < Tiles_Test
 		assert_not_nil Equation.new(' 21)')
 		assert_not_nil Equation.new('- 21')
 		assert_not_nil Equation.new('- 2 + 1* 3-')
+	end
+	def test_variable
+		# Test Object
+		thing = Thing.new
+		thing.add_reference "item", Thing.new , :add_then_reference => true
+		thing.item.add_variable "value", 0
+		eq = Equation.new("value + 2")
+		assert eq.parse_failure? , "Equation did not resolve => #{Equation.last_failure_reason}"
+		eq.source = thing.item
+		assert_equal 2, eq.resolve
+		thing.item.value.set 3
+		assert_equal 5, eq.resolve
+		thing.item.add_variable "output", 0
+		eq = Equation.new("output = value + 2")
+		assert eq.parse_failure? , "Equation did not resolve => #{Equation.last_failure_reason}"
+		eq.source = thing.item
+		eq.resolve
+		assert_equal 5, thing.item.output
+		eq = Equation.new("item#output = item#value + 2")
+		assert eq.parse_failure? , "Equation did not resolve => #{Equation.last_failure_reason}"
+		eq.source = thing
+		eq.resolve
+		assert_equal 5, thing.item.output
 	end
 end
