1e281cd4750eda8ca48a6dd6748e2c52c1bbba0a
rejuxst <wer123hitech@gmail.com>
2013-02-09 20:47:04 -0500

    Extended manager and channel interaction. Commiting before large changes to UI in test application

--- lib/manager.rb
+++ lib/manager.rb
@@ -6,31 +6,50 @@ module Tiles::Application ; end
 end
 class Tiles::Application::Manager
 
+	private :method
+	private :send 
+	private	:singleton_class
+	private :singleton_method_removed
+	private	:singleton_method_added
+	########
+	public	
+	#######
 	def create_connection(ui,channel_class)
 		raise "Input UI is not a UI it is a #{ui.class}" unless ui.is_a? UI
 		if valid_channels.has_key channel_class
-			channel_instances[ui] = valid_channels[channel_class].call(ui.method(:inbound_package))
+			key= 0 
+			key= Random.new(object_id).rand(1000000) while key != 0 && channel_instances.has_key?(key)
+			channel_instances[key] = valid_channels[channel_class].call(ui.method(:inbound_package))
+			outbound = (method :outbound_package)
+			ui.outbound_package_method= lambda { |package| outbound.call(key,package) }
 			true
 		else
 			false
 		end
 	end
-	def regisier_new_channel(channel_class_new)
-		valid_channels[channel_class.name] = channel_class
+	def register_new_channel(channel_class, channel_class_new)
+		valid_channels[channel_class.to_s] = channel_class_new.to_proc
 		true
 	end
-	def outbound_message(ui,package)
-		channel_instances[ui].outbound_package package
-	end
-	private 
+
 	def initialize(opts = {})
-		# Securing a Manager instance
-		[
-		:method, :send, 
-		:singleton_class, :singleton_method_removed, :singleton_method_added
-		:private,:public
-		].each { |meth| private meth } 
 	end
+	private
+	def outbound_package(key_target,package)
+		#raise "Too many invalid key access attempts" if invalid_count > 1000
+		#return add_invalid_count unless channel_instances.has_key? key_target
+		channel_instances[key_target].outbound_package package
+		#reset_invalid_count
+	end
+#	def add_invalid_count
+#		(@invalid = (@invalid || 0) + 1) == 0
+#	end
+#	def reset_invalid_count
+#		(@invalid = 0) == 0
+#	end
+#	def invalid_count
+#		@invalid ||= 0
+#	end
 	def valid_channels
 		@valid_channels ||= {}
 	end
@@ -40,4 +59,5 @@ class Tiles::Application::Manager
 	def channel_instances
 		@channel_instances ||= {}
 	end
+
 end
