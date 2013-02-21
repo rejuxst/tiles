83cd5643c4905d2a5b2c4ee4ecf12fc26c8bb3f5
rejuxst <wer123hitech@gmail.com>
2013-02-21 15:44:16 -0500

    added aliasing and fixed compiler issues

--- test/blocktest/languagecompiler_test.rb
+++ test/blocktest/languagecompiler_test.rb
@@ -9,13 +9,17 @@ class Test_LanguageCompiler < Test::Unit::TestCase
 	#Treetop.load File.join(File.dirname(__FILE__),"..","..","lib","treetop")
   def test_interactive
 	assert_not_nil a = LanguageCompiler.parse( ' $TERMINAL_CHARACTERS : /lmn/ ; ' )
-	assert_not_nil c = LanguageCompiler.parse(
+	assert_not_nil b = LanguageCompiler.parse(
 				'@var : A+;
 				% Comment
 				@bar : B+;
 				$TERMINAL_CHARACTERS : /[\s_";()]/ ;
 				'
 			), LanguageCompiler.failure_reason
+	assert_not_nil dict1 = LanguageCompiler.parse( 
+					'water anger money politics trouble: {@A-} & {Dmu-} & 
+					{@M+ or (R+ & Bs+)} & (J- or Os- or (Ss+ & (({@CO-} & {C-}) or R-)) or SIs-);' 
+				)
 	assert_not_nil dict0 = LanguageCompiler.parse(
      				'
     				 I    : J- or O- or (Sp+ & (({@CO-} & {C-}) or R-)) or SIp-;
@@ -24,6 +28,13 @@ class Test_LanguageCompiler < Test::Unit::TestCase
     				 the  : D+;
      				 dog  : {@A-} & Ds- & {@M+ or (R+ & Bs+)} & (J- or Os- or (Ss+ & (({@CO-} & {C-}) or R-)) or SIs-);
      				'
+    			), LanguageCompiler.failure_reason
+	assert_not_nil dict2 = LanguageCompiler.parse(
+<<EOF
+")" "%" "," "." ":" ";" "?" "!" "''" "'" "'s" "'re" "'ve" "'d" "'ll" "'m" : RP-;
+"(" "$" "``" : LP-;
+<CLAUSE>: {({@COd-} & C-) or ({@CO-} & (Wd- & {CC+})) or [Rn-]};
+EOF
     			), LanguageCompiler.failure_reason
 	LanguageCompiler.generate_instance_dictionary English, dict0
 	a = English.parse "I ran with the dog"
