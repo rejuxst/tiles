47e264cddf62d72a8bf7f3929dc32fa1d6da4c33
rejuxst <wer123hitech@gmail.com>
2013-02-20 19:15:55 -0500

    Checkpoint with working ling. base code rewrite

---
+++ lib/language.rb
@@ -0,0 +1,51 @@
+require 'database'
+require 'pry'
+require 'linguistics'
+class Language
+	def self.load_parser
+		Treetop.load File.join(File.dirname(__FILE__),"lang/#{self.name.downcase}parser")
+		@parser = eval("#{self.name}LanguageParser.new")
+	end
+	def self.parse(string)
+		return @parser.parse(string)
+	end
+	def self.parser
+		@parser
+	end
+	def self.inherited(subclass)
+		subclass.load_parser
+	end
+	class Grammar
+		extend Database	
+		def self.add_class(wordclass,equation)
+			add_reference(wordclass,equation) {|src,tar| ::Linguistics.parse(tar)}
+		end
+		def self.[]=(key,value)
+			add_class(key,value)
+		end	
+	end
+end
+class Language::Word < Treetop::Runtime::SyntaxNode
+	attr_accessor :wordclass
+	def self.set_language(lang)
+		@language ||= lang
+	end
+	def self.language
+		@language
+	end
+	def get_dictionary_entry
+		self.class.language::Dictionary[word.strip]
+	end
+	def get_grammar_entry
+		get_dictionary_entry.grammar
+	end
+	def grammar_equation
+		self.class.language::Grammar[get_grammar_entry]
+	end
+	def links
+		wordclass.links
+	end
+	def word
+		text_value.strip
+	end
+end
