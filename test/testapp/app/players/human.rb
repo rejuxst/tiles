680192e79626e411a73e27b83c22d6295127e033
rejuxst <wer123hitech@gmail.com>
2015-07-05 20:35:17 -0700

    prettify

--- test/testapp/app/players/human.rb
+++ test/testapp/app/players/human.rb
@@ -1,32 +1,40 @@
-require 'mixins/ncurses/ncurses_ui'
+require 'tiles/mixins/ncurses/ncurses_ui'
+
 class Human < Player
   def init(arghash = {})
   end
 end
+
 class SuperHuman_DEBUG < Human
   attr_reader :ui
+
   def init(arghash = {})
   	super
   	@useshell = false;
   end
-  class UI <   ::Ncurses::UI
+
+  class UI < ::Ncurses::UI
+
   	def take_turn
   		nil until (event = request_inbound_package).match(/[wasd]/)
   		case event
-  			when 'w' then  Proc.new { Move.down(controls["character"])  } 
+  			when 'w' then  Proc.new { Move.down(controls["character"])  }
   			when 'd' then  Proc.new { Move.right(controls["character"])  }
-  			when 'a' then  Proc.new { Move.left(controls["character"]) } 
-  			when 's' then  Proc.new { Move.up(controls["character"])    } 
-  			when 't' then  :empty 
+  			when 'a' then  Proc.new { Move.left(controls["character"]) }
+  			when 's' then  Proc.new { Move.up(controls["character"])    }
+  			when 't' then  :empty
   		end
-  		
+
   	end
+
   	def request_inbound_package
   		str = super
   		str.is_a?(String) ? str : ''
   	end
   end
-  class Channel < ::Ncurses::Channel; end	
+
+  class Channel < ::Ncurses::Channel; end
+
   class View < ::Ncurses::View
   	attr_accessor :iline,:stream
   	def initialize(opts = {})
@@ -59,14 +67,14 @@ class SuperHuman_DEBUG < Human
   		Ncurses.refresh
   		render_shell package
   	end
+
   	def render_mainwindow game
-  		game.map.tiles.each do |k,t|
-  				Ncurses.setpos(k[0],k[1])
-  				Ncurses.addstr (
-  					(t.find_if {|t| t.class <= Actor} || t).ASCII
-  				)
+  		game.map.tiles.each do |k, t|
+  				Ncurses.setpos(k[0], k[1])
+  				Ncurses.addstr((t.find_if {|_t| _t.class <= Actor} || t).ASCII)
   			end
   	end
+
   	def render_shell game
   		rowtop = game.map.rows + 3
   		r = 0;# stream.length;
@@ -78,40 +86,44 @@ class SuperHuman_DEBUG < Human
   		Ncurses.setpos(rowtop+stream.length,0)
   		Ncurses.addstr("TILES>>> #{iline}")
   	end
+
   	def render_character_ui game
   		left_col = game.map.columns + 3
-  		Ncurses.setpos(0,left_col)
-  		Ncurses.addstr("Player 1")
-  		Ncurses.setpos(1,left_col)	
-  		Ncurses.addstr("HP: #{'X' * @owner.character.hp}#{'-' * ( @owner.character.max_hp -  @owner.character.hp )}")
+  		Ncurses.setpos 0, left_col
+  		Ncurses.addstr "Player 1"
+  		Ncurses.setpos 1, left_col
+			hp_str = 'X' * @owner.character.hp + '-' * (@owner.character.max_hp -  @owner.character.hp)
+  		Ncurses.addstr("HP: #{hp_str}")
   	end
+
   	def sendchar(input)
   		return process_shell if input == "\n".ord or input == "\r".ord or input == 10
   		iline << input.chr;
   		return true;
   	end
+
   	def process_shell
   		stream.delete_at(1) while stream.length >= @max_length
   		if iline == 'exit' or iline == 'exit()'
   			stream << iline
-  			return false;	
+  			return false;
   		end
   		return true if iline.empty?
   		stream << "TILES>>> #{iline}"
   		begin
   			binding.pry and initialize() and render() if(iline == 'pry')
-  			n = "#{eval(iline)}" 
-  		rescue 
+  			n = "#{eval(iline)}"
+  		rescue
   			n = "#{$!}"
   			Ncurses.close_screen
   			initialize
   			render
   		end
-  		
+
   		stream << n
   		iline.clear
   		return true;
   	end
-  end	
-  
+  end
+
 end
